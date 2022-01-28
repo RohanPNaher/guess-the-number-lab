192aa05d501a9e4b4a16f6d2b55dd43d1583161e
RohanPNaher <RohanPNaher@email.com>
2022-01-28 17:46:39 -0500

    add and initialized prevGuesses to game object

--- app.js
+++ app.js
@@ -6,5 +6,7 @@ const game = {
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-  }
-}
+  },
+  prevGuesses: [],
+  
+};
